5d743ef065032d1f2bc5da9d3799b300e3321b87
Timhung803 <timhkt@gmail.com>
2022-08-17 23:15:15 +1200

    Added icons and texts on the header and styled them.

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./components/HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
 function Header() {
   return (
     <div className="header">
@@ -12,6 +19,11 @@ function Header() {
         </div>
       </div>
       <div className="header__right"></div>
+      <HeaderOption Icon={HomeIcon} title="Home" />
+      <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+      <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+      <HeaderOption Icon={ChatIcon} title="Messaging" />
+      <HeaderOption Icon={NotificationsIcon} title="Notifications" />
     </div>
   );
 }
